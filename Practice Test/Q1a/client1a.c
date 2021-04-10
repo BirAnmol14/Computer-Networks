38a1672b46f8a2718d741360768d20cec61ec876
Bir Anmol Singh <f20180261@pilani.bits-pilani.ac.in>
2021-04-10 14:34:42 +0530

    Q1a

---
+++ Practice Test/Q1a/client1a.c
@@ -0,0 +1,169 @@
+//BASIC HEADERS
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+//SOCKET HEADERS
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+//FILE 
+#include <fcntl.h>
+//MISC
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+
+
+#define FILENAME "input.txt"
+#define ADDR "127.0.0.1"
+#define PORT 12121
+#define TIMEOUT 2
+#define P_SIZE 100
+
+typedef struct pkt{
+	int  size;
+	int seq_no;
+	char lst_pkt;// '1' -> last ; '0' -> not last
+	char TYPE;// 'D'->data, 'A'->ack
+	char payload[P_SIZE];
+}PKT;
+int gsock;
+int g_pktSiz;
+int gpayLoad;
+PKT last;
+PKT curr;
+void die(char * c){
+		perror(c);
+		exit(1);
+}
+void backup(PKT *p1,PKT *p2){
+		int payloadSize = gpayLoad;
+		p2->size=p1->size;
+		p2->seq_no = p1->seq_no;
+		p2->lst_pkt = p1->lst_pkt;
+		p2->TYPE = p1->TYPE;
+		memset(p2->payload,'\0',payloadSize);
+		strcpy(p2->payload,p1->payload);
+		memset(p1->payload,'\0',payloadSize);
+}
+void printStatus(int type, PKT *p){
+	if(type == 0){
+			//SENT PKT
+			printf("SENT PKT: Seq. No. = %d,Packet Size= %d ,Payload Size = %d\n",ntohl(p->seq_no),sizeof(PKT),ntohl(p->size));
+			return;
+	}
+	if(type == 1){
+			//RE_TRANSMIT PKT
+			printf("RE_TRANSMIT PKT: Seq. No. = %d,Packet Size= %d ,Payload Size = %d\n",ntohl(p->seq_no),sizeof(PKT),ntohl(p->size));
+			return;
+	}
+	if(type == 2){
+			//ACK PKT
+			printf("ACK PKT: Seq. No. = %d\n",ntohl(p->seq_no));
+			return;
+	}
+}
+void SEND(int sock,int PACKET_SIZE,int type){
+	int r;
+	if(type == 0){
+		backup(&curr,&last);
+		//send data
+		r = send(sock,&last,sizeof(last),0);
+		if(r<0){die("SEND Error");}
+		printStatus(type,&last);
+	}else if(type == 1){
+		//send data
+		r = send(sock,&last,sizeof(last),0);
+		if(r<0){die("SEND Error");}
+		printStatus(type,&last);
+	}
+}
+void handle(int signo){
+		if(signo == SIGALRM){
+			//TIMEOUT
+			alarm(TIMEOUT);
+			SEND(gsock,g_pktSiz,1);
+		}
+}
+int main(){
+	int PACKET_SIZE = P_SIZE;
+	g_pktSiz = PACKET_SIZE;
+	int headerSize =  sizeof(curr.size) +  sizeof(curr.seq_no)+ sizeof(curr.lst_pkt)+sizeof(curr.TYPE);
+	int payloadSize = PACKET_SIZE;
+	gpayLoad = payloadSize;
+	if(payloadSize<=0){
+		die("Invalid Packet Size");
+	}
+	
+	//SET ALARM HANDLER
+	struct sigaction act;
+	act.sa_handler = handle;
+	act.sa_flags = SA_RESTART; // restart the recv call after interrupt handled
+	sigaction(SIGALRM,&act,NULL);
+	memset(curr.payload,'\0',payloadSize);
+	memset(last.payload,'\0',payloadSize);
+	//create TCP socket
+	int sock = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
+	if(sock<0){die("Socket could not be created");}
+	gsock  = sock;
+	//Set server addr
+	struct sockaddr_in serv;
+	serv.sin_family =AF_INET;
+	serv.sin_port = htons(PORT);
+	serv.sin_addr.s_addr = inet_addr(ADDR);
+	
+	//Connect to server
+	int r = connect(sock,(struct sockaddr *)&serv,sizeof(serv));
+	if(r<0){die("Connect error");}
+	sleep(2);
+	//Open file
+	int fd = open(FILENAME,O_RDONLY);
+	if(fd<0){die("Unable to read file");}
+	
+	int n;
+	int seq = 0;
+	int done = 0;
+	while(!done && (n=read(fd,curr.payload,payloadSize))>=0){
+			curr.seq_no = htonl(seq);
+			curr.size = htonl(n);
+			if(n==0){
+				//EOF
+				curr.lst_pkt = '1';
+				done = 1;
+			}else{
+				curr.lst_pkt ='0';
+			}
+			curr.TYPE = 'D';
+			SEND(sock,PACKET_SIZE,0);
+			//recv ACK
+			alarm(TIMEOUT);
+			PKT rcv;
+			r = recv(sock,&rcv,sizeof(rcv),0);
+			if(r<0){
+					if(errno == EINTR){
+						//alarm went off - auto restart at recv
+					}else{
+							die("Recv error");
+					}
+			}
+			if(r==0){
+				puts("Server terminated Connection");
+				break;
+			}
+			alarm(0);
+			if(rcv.TYPE == 'A' && last.seq_no == rcv.seq_no){
+					printStatus(2,&rcv);
+			}else{
+				die("SERVER CORRUPT RESPONSE");
+			}
+			seq+=sizeof(last);
+			usleep(100 * 1000); // 100ms sleep
+	}
+	if(n<0){die("READ ERROR");}
+	close(fd);
+	close(sock);
+}
---
+++ Practice Test/Q1a/server1a.c
@@ -0,0 +1,164 @@
+//BASIC HEADERS
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+//SOCKET HEADERS
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+//FILE 
+#include <fcntl.h>
+//MISC
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+
+#define ADDR "127.0.0.1"
+#define PORT 12121
+#define P_SIZE 100
+
+typedef struct pkt{
+	int size;
+	int seq_no;
+	char lst_pkt;// '1' -> last ; '0' -> not last
+	char TYPE;// 'D'->data, 'A'->ack
+	char payload[P_SIZE];
+}PKT;
+
+void die(char * c){
+		perror(c);
+		exit(1);
+}
+int randomDrop(int PDR){
+	srand(time(0));
+	int r = rand()%10+1;//1 to 10
+	if(r<=PDR){
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+void printStatus(int type, PKT *p){
+	if(type == 0){
+			//RCVD PKT
+			printf("RCVD PKT: Seq. No. = %d,Packet Size= %d ,Payload Size = %d\n",ntohl(p->seq_no),sizeof(PKT),ntohl(p->size));
+			return;
+	}
+	if(type == 1){
+			//SENT ACK PKT
+			printf("SENT ACK: Seq. No. = %d\n",ntohl(p->seq_no));
+			return;
+	}
+	if(type == 2){
+			//DROP PKT
+			printf("DROP PKT: Seq. No. = %d\n",ntohl(p->seq_no));
+			return;
+	}
+}
+
+
+int main(){
+	int PACKET_SIZE = P_SIZE;
+	int PDR = 7;// 1-> 10 %, 2-> 20% and so on
+	PKT ack,rcv;	
+	int headerSize =  sizeof(ack.size) +  sizeof(ack.seq_no)+ sizeof(ack.lst_pkt)+sizeof(ack.TYPE);
+	int payloadSize = PACKET_SIZE ;
+	int LISTQ=5;
+	if(payloadSize<=0){
+		die("Invalid Packet Size");
+	}
+	memset(ack.payload,'\0',payloadSize);
+	memset(rcv.payload,'\0',payloadSize);
+	//create TCP socket
+	int lsock = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP);
+	if(lsock<0){die("Socket could not be created");}
+	
+	//Set server addr
+	struct sockaddr_in serv;
+	serv.sin_family =AF_INET;
+	serv.sin_port = htons(PORT);
+	serv.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	//bind the address
+	int r = bind(lsock,(struct sockaddr *)&serv,sizeof(serv));
+	if(r<0){die("Bind error");}
+	
+	//Listen for connections on server
+	r = listen(lsock,LISTQ);
+	if(r<0){die("Listen error");}
+	
+	printf("Server Up on %s : %d\n",inet_ntoa(serv.sin_addr),ntohs(serv.sin_port));
+	int file =1;
+	while(1){
+		struct sockaddr_in cli; int len;
+		int confd= accept(lsock,(struct sockaddr *)&cli,&len);
+		if(confd<0){die("Accept Failed");}
+		printf("Connected to Client %s : %d\n",inet_ntoa(cli.sin_addr),ntohs(cli.sin_port));
+		char * buf;
+		buf = malloc(1*sizeof(char));
+		int last = 0;
+		int size=1;
+		int n;
+		int done = 0;
+		
+		//rcv packets from client till the end
+		while(!done && (n = recv(confd,&rcv,sizeof(rcv),0))>0){
+			printStatus(0,&rcv);
+			
+			
+			int drop = randomDrop(PDR);
+			if(drop){
+					//pkt drop 
+					printStatus(2,&rcv);
+					continue ;
+			}
+			
+			if(rcv.TYPE == 'D'){
+					//DATA pkt rcv
+					if(rcv.lst_pkt == '1'){
+						//last pkt
+						done = 1;
+					}else{
+						done =0 ;
+					}
+					printStatus(0,&rcv);
+					size = size + ntohl(rcv.size);
+					buf = realloc(buf,size);
+					strcpy(buf+last,rcv.payload);
+					last = last + ntohl(rcv.size);
+					memset(rcv.payload,'\0',payloadSize);
+					
+					//SEND ACK
+					ack.seq_no = rcv.seq_no;
+					ack.size = htonl(0);
+					memset(ack.payload,'\0',payloadSize);
+					ack.lst_pkt = rcv.lst_pkt;
+					ack.TYPE = 'A';
+					
+					r = send(confd,&ack,sizeof(ack),0);
+					if(r<0){die("SEND Error");}
+					printStatus(1,&ack);
+			}else{
+				close(confd);
+				close(lsock);
+				die("Corrupt Client");
+			}
+		}
+		if(n<0){die("Recv error");}
+		//write buf to output file
+		buf[size]='\0';
+		char fname[256];
+		sprintf(fname,"output%d.txt",file);
+		FILE * fp = fopen(fname,"w");
+		fprintf(fp,"%s",buf);
+		fclose(fp);
+		file++;
+	}
+	
+	close(lsock);
+}
